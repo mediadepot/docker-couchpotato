c4ac41cddc2da654f8c9ec9762f59c13ae35cb28
Jason Kulatunga <jk17@ualberta.ca>
2015-11-11 13:43:33 -0800

    update to use mounts and env variables, specify the username

--- start.sh
+++ start.sh
@@ -3,8 +3,7 @@ if [ ! -f /srv/couchpotato/config/config.ini ]; then
 	#generate the config file for the first time using docker-gen
 
 	cheetah fill --oext cfg --env /srv/couchpotato/config/couchpotato
+	chown -R depot:depot /srv/couchpotato
 fi
 
-/usr/bin/python /srv/couchpotato/app/CouchPotato.py \
-	--data_dir /srv/couchpotato/data \
-	--config_file /srv/couchpotato/config/couchpotato.cfg
+su -c "/usr/bin/python /srv/couchpotato/app/CouchPotato.py --data_dir /srv/couchpotato/data --config_file /srv/couchpotato/config/couchpotato.cfg" depot
