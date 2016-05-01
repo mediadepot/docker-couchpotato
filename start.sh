9ae7b9a2dc1bed208860dfe6d7bc3cad22ec45cf
Jason Kulatunga <jk17@ualberta.ca>
2016-05-01 10:46:16 -0600

    using new container model.

--- start.sh
+++ start.sh
@@ -7,7 +7,13 @@ sh /srv/config.sh
 chown mediadepot:mediadepot -R /srv/couchpotato
 
 # download the latest version of CouchPotato
-su -c "git clone https://github.com/RuudBurger/CouchPotatoServer.git /srv/couchpotato/app" mediadepot
+[[ ! -d /srv/couchpotato/app/.git ]] && su -c "git clone https://github.com/RuudBurger/CouchPotatoServer.git /srv/couchpotato/app" mediadepot
+
+# opt out for autoupdates using env variable
+if [ -z "$ADVANCED_DISABLEUPDATES" ]; then
+	# update the application
+	cd /srv/couchpotato/app/ && su -c "git pull" mediadepot
+fi
 
 # run CouchPotato
 su -c "/usr/bin/python /srv/couchpotato/app/CouchPotato.py --data_dir /srv/couchpotato/data --config_file /srv/couchpotato/config/couchpotato.cfg" mediadepot
